b2956281dfe04cd5ef6a10cc510de82a48df2da7
Victor Augusto Gomes Costa <victor.gomes@GFP.LOCAL>
2025-03-31 17:54:38 -0300

    Configurações finais DB

--- automacoes/automacoes/settings.py
+++ automacoes/automacoes/settings.py
@@ -78,10 +78,15 @@ WSGI_APPLICATION = 'automacoes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'automacoes_db',
+        'USER': 'postgres',
+        'PASSWORD': 'fisco121*',
+        'HOST': 'localhost',  
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
